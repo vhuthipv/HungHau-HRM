57e7d1884f3a8e1c46d487dc669c83b6f31c7838
vhuthipv <thipv@vhu.edu.vn>
2025-11-21 17:48:12 +0700

    Configure Vite for React with base path
    Added Vite configuration for React with a specific base path.

--- vite.config.ts
+++ vite.config.ts
@@ -21,3 +21,10 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/HungHau-HRM/', // TÊN REPO phải chính xác
+})
